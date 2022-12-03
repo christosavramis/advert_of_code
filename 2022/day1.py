d2941beb83a0afdf841475c6ebab0df92ba0a6f3
Christos Avramis <xristosav1999@hotmail.com>
2022-12-03 20:39:27 +0200

    Update day1.py

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,3 +1,5 @@
+import pandas as pd
+data = pd.read_csv('https://raw.githubusercontent.com/christosavramis/advert_of_code/main/2022/day1.txt', header=None, skip_blank_lines=False).fillna(0).to_numpy().flatten()
 sum = 0
 max = 0
 for dat in data:
