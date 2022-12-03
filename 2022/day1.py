e51a5d01658660a8821a4d88d71cc914d0e6e955
Christos Avramis <xristosav1999@hotmail.com>
2022-12-03 20:48:14 +0200

    Update day1.py

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,12 +1,12 @@
 import pandas as pd
 data = pd.read_csv('https://raw.githubusercontent.com/christosavramis/advert_of_code/main/2022/day1.txt', header=None, skip_blank_lines=False).fillna(0).to_numpy().flatten()
-sum = 0
-max = 0
+sums = []
+sumC = 0
 for dat in data:
   if (dat == 0):
-    if (max < sum):
-      max = sum
-    sum = 0
+    sums.append(sumC)
+    sumC = 0
   else:
-    sum = sum + dat
-max
+    sumC = sumC + dat
+sums = sorted(sums, reverse=True)
+print("A ",  sums[0], "B ", sum(sums[:3]))
